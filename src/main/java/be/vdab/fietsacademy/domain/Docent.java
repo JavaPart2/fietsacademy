9dc1d7b5fa01cd5fed021b9006dbe2988ad0a83c
Marc J <john.doe.gmail.com>
2020-12-21 16:06:38 +0100

    Wijzigen t/m 9.3

--- src/main/java/be/vdab/fietsacademy/domain/Docent.java
+++ src/main/java/be/vdab/fietsacademy/domain/Docent.java
@@ -1,22 +1,39 @@
 package be.vdab.fietsacademy.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Entity
 @Table(name = "docenten")
 public class Docent {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String voornaam;
     private String familienaam;
+    @Enumerated(EnumType.STRING)
+    private Geslacht geslacht;
     private BigDecimal wedde;
     private String emailAdres;
 
+    public Docent(String voornaam, String familienaam, Geslacht geslacht, BigDecimal wedde, String emailAdres) {
+        this.voornaam = voornaam;
+        this.familienaam = familienaam;
+        this.geslacht = geslacht;
+        this.wedde = wedde;
+        this.emailAdres = emailAdres;
+    }
+
+    protected Docent() {
+    }
+
     public long getId() {
         return id;
+    }
+
+    public Geslacht getGeslacht() {
+        return geslacht;
     }
 
     public String getVoornaam() {
@@ -33,5 +50,13 @@ public class Docent {
 
     public String getEmailAdres() {
         return emailAdres;
+    }
+
+    public void opslag(BigDecimal percentage){
+        if (percentage.compareTo(BigDecimal.ZERO) <= 0){
+            throw new IllegalArgumentException();
+        }
+        var factor = BigDecimal.ONE.add(percentage.divide(BigDecimal.valueOf(100)));
+        wedde = wedde.multiply(factor).setScale(2, RoundingMode.HALF_UP);
     }
 }
--- src/test/java/be/vdab/fietsacademy/repositories/JPADocentRepositoryTest.java
+++ src/test/java/be/vdab/fietsacademy/repositories/JPADocentRepositoryTest.java
@@ -1,10 +1,16 @@
 package be.vdab.fietsacademy.repositories;
 
+import be.vdab.fietsacademy.domain.Docent;
+import be.vdab.fietsacademy.domain.Geslacht;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -13,14 +19,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Import(JPADocentRepository.class)
 public class JPADocentRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
     private final JPADocentRepository jpaDocentRepository;
+    private static final String DOCENTEN = "docenten";
+    private Docent docent;
+    private final EntityManager manager;
 
-    public JPADocentRepositoryTest(JPADocentRepository jpaDocentRepository) {
+    public JPADocentRepositoryTest(JPADocentRepository jpaDocentRepository, EntityManager manager) {
         this.jpaDocentRepository = jpaDocentRepository;
+        this.manager = manager;
     }
 
     private long idVanTestMan(){
         return super.jdbcTemplate.queryForObject(
                 "select id from docenten where voornaam = 'testM'", long.class);
+    }
+
+    private long idVanTestVrouw(){
+        return super.jdbcTemplate.queryForObject(
+                "select id from docenten where voornaam = 'testV'", long.class);
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        docent = new Docent("test", "test",
+                Geslacht.MAN, BigDecimal.TEN, "test@test.be");
     }
 
     @Test
@@ -32,5 +53,32 @@ public class JPADocentRepositoryTest extends AbstractTransactionalJUnit4SpringCo
     @Test
     void findByOnbestaandeId(){
         assertThat(jpaDocentRepository.findById(-1)).isNotPresent();
+    }
+
+    @Test
+    void man(){
+        assertThat(jpaDocentRepository.findById(idVanTestMan())
+        .get().getGeslacht()).isEqualTo(Geslacht.MAN);
+    }
+
+    @Test
+    void vrouw(){
+        assertThat(jpaDocentRepository.findById(idVanTestVrouw())
+                .get().getGeslacht()).isEqualTo(Geslacht.VROUW);
+    }
+
+    @Test
+    void create(){
+        jpaDocentRepository.create(docent);
+        assertThat(docent.getId()).isPositive();
+        assertThat(super.countRowsInTableWhere(DOCENTEN, "id = " + docent.getId())).isOne();
+    }
+
+    @Test
+    void delete(){
+        var id = idVanTestMan();
+        jpaDocentRepository.delete(id);
+        manager.flush();
+        assertThat(super.countRowsInTableWhere(DOCENTEN, "id = " + id)).isZero();
     }
 }
